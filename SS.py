dc399fcc752590420459dd9bb5be0c5c7149534e
Dorsa-Sharifi <dorsa.sharifi@gmail.com>
2025-01-28 21:40:12 +0330

    Semantic Stack Added

---
+++ SS.py
@@ -0,0 +1,14 @@
+class SS:
+    def __init__(self):
+        self.stack = []
+        pass
+    def push(self, element):
+        self.stack.append(element)
+
+    def pop(self):
+        if len(self.stack) > 0:
+            return self.stack.pop()
+        else:
+            return None
+    def is_Empty(self):
+        return len(self.stack) == 0
--- codegen.py
+++ codegen.py
@@ -1,3 +1,79 @@
 class Codegen:
     def __init__(self):
         pass
+
+
+    def Push_ID(self):
+        pass
+    def Add_Var_SS(self):
+        pass
+    def Push_NUM(self):
+        pass
+    def Create_Arr(self):
+        pass
+    def Params_Declaration(self):
+        pass
+    def Func_Beginning(self):
+        pass
+    def Search_For_Return(self):
+        pass
+    def Found_Return(self):
+        pass
+    def Return_Main(self):
+
+        pass
+
+    def Func_Ending(self):
+        pass
+
+    def Array_Args(self):
+        pass
+    def Next_Scope(self):
+        pass
+    def Prev_Scope(self):
+        pass
+    def Remove_Data_From_SS(self):
+        pass
+    def End_of_Loop_Break(self):
+        pass
+    def Save(self):
+        pass
+    def Jpf_Save(self):
+        pass
+    def Jump(self):
+        pass
+    def Label(self):
+
+        pass
+    def New_Break(self):
+        pass
+    def While_Jumps(self):
+        pass
+    def Return_Scope_with_Break(self):
+        pass
+    def Save_Return_Point(self):
+
+        pass
+    def Push_Index(self):
+        pass
+    def Push_ID_Addr(self):
+
+        pass
+    def Assignment(self):
+        pass
+    def Array_Indices(self):
+        pass
+    def Pop_Relation(self):
+        pass
+    def Save_Operation(self):
+        pass
+    def Multiply_Division(self):
+        pass
+    def Negative(self):
+        pass
+    def Push_Num(self):
+        pass
+    def Func_Output(self):
+        pass
+    def Func_Call(self):
+        pass
