2715bf5aa8dc602660f6bea5244acbd5a40afc21
Dorsa-Sharifi <dorsa.sharifi@gmail.com>
2025-01-30 15:18:40 +0330

    Some of the functions are complete

--- SS.py
+++ SS.py
@@ -12,3 +12,12 @@ class SS:
             return None
     def is_Empty(self):
         return len(self.stack) == 0
+
+    def pop_with_num(self, number):
+        if len(self.stack) >= number:
+            a, b, c = self.stack.pop(), self.stack.pop(), self.stack.pop()
+            return a, b, c
+        else:
+            return None
+    def access_members(self, index):
+        return self.stack[index]
--- codegen.py
+++ codegen.py
@@ -1,21 +1,107 @@
+from operator import index
+
 from PB import PB
 from SS import SS
 
 
+
+#TODO Get back for printing possible errors
 class Codegen:
     def __init__(self):
+        self.index = 0
+        self.current_scope = 0
         self.ss = SS()
         self.pb = PB()
+
+    def run(self, action):
         pass
 
-    def Push_ID(self):
+    def Push_ID(self, id):
+        #Save Id for future uses
+        self.ss.push(id)
 
-        pass
     def Add_Var_SS(self):
+        #Scope Managing
+        top = self.ss.pop()
+        addr = self.get_temp()
+        scope = self.current_scope
+        #TODO Add the given popped value to the symbol table
+
+    def Push_Num(self, num):
+        self.ss.push(num)
+    def Array_Args(self):
         pass
-    def Push_NUM(self):
+    def Next_Scope(self):
         pass
+    def Prev_Scope(self):
+        pass
+    def Remove_Data_From_SS(self):
+        self.ss.pop()
+
+    def End_of_Loop_Break(self):
+
+        pass
+    def Save(self):
+        self.ss.push(self.index)
+        self.index += 1
+        pass
+    def Jpf_Save(self):
+        destination, operator1 = self.ss.pop_with_num(2)
+        target = self.index + 1
+        self.pb.Add_Code('JPF', operator1, target,'' ,destination)
+        self.index += 1
+
+    def Jump(self):
+        destination = int(self.ss.pop())
+        self.pb.Add_Code('JP', {self.index},'','',destination)
+    def Label(self):
+        self.ss.push(self.index)
+
+    def While_Jumps(self):
+        self.pb.Add_Code('JPF', {self.ss.access_members(-2)}, {self.index + 1}, '',
+                         self.ss.access_members(-1))
+        self.pb.Add_Code('JP', self.ss.access_members(-3), '','',self.index)
+        self.index += 1
+        self.ss.pop()
+        self.ss.pop()
+        self.ss.pop()
+        pass
+    def Return_Scope_with_Break(self):
+        pass
+    def Save_Return_Point(self):
+
+        pass
+    def Push_Index(self, index):
+        pass
+    def Push_ID_Addr(self):
+
+        pass
+    def Assignment(self):
+        pass
+    def Array_Indices(self):
+        pass
+    def Pop_Relation(self, relation):
+        self.ss.push(relation)
+        pass
+    def Save_Operation(self):
+        addr = self.get_temp()
+        operand2, operator, operand1 = self.ss.pop_with_num(3)
+        #TODO: a list of operands + add_code function
+        self.ss.push(addr)
+        pass
+    def Multiply_Division(self):
+        pass
+    def Negative(self):
+        pass
+
+    def Func_Output(self):
+        pass
+    def Func_Call(self):
+        pass
+
+    #Related to Scoping
     def Create_Arr(self):
+        #TODO:  Creates an array entry in the symbol table with the previously pushed number as its size.
         pass
     def Params_Declaration(self):
         pass
@@ -26,60 +112,14 @@ class Codegen:
     def Found_Return(self):
         pass
     def Return_Main(self):
-
         pass
-
     def Func_Ending(self):
-        pass
-
-    def Array_Args(self):
-        pass
-    def Next_Scope(self):
-        pass
-    def Prev_Scope(self):
-        pass
-    def Remove_Data_From_SS(self):
-        pass
-    def End_of_Loop_Break(self):
-        pass
-    def Save(self):
-        pass
-    def Jpf_Save(self):
-        pass
-    def Jump(self):
-        pass
-    def Label(self):
-
         pass
     def New_Break(self):
         pass
-    def While_Jumps(self):
-        pass
-    def Return_Scope_with_Break(self):
-        pass
-    def Save_Return_Point(self):
 
-        pass
-    def Push_Index(self):
-        pass
-    def Push_ID_Addr(self):
 
-        pass
-    def Assignment(self):
-        pass
-    def Array_Indices(self):
-        pass
-    def Pop_Relation(self):
-        pass
-    def Save_Operation(self):
-        pass
-    def Multiply_Division(self):
-        pass
-    def Negative(self):
-        pass
-    def Push_Num(self):
-        pass
-    def Func_Output(self):
-        pass
-    def Func_Call(self):
-        pass
+    #Handy Functions
+    def get_temp(self):
+        #TODO
+        return None
